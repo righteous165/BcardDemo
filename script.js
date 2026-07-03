1016807c84bad08b1d6afa33a1b9cbe0f20197df
Mahama Radiatu <radiazakaria19@gmail.com>
2026-07-03 14:03:23 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Personalize the birthday card message
     cardMessage.innerHTML =
-      `Happy Birthday, ${name}!<br><br> Thank you for always being that amazing person who brings joy to everyone around you. Your kindness, laughter, and warmth make the world a better place.<br> Continue to shine and spread happiness wherever you go. May this year bring you countless blessings, unforgettable memories, and all the love your heart can hold.<br> On this day, I wish you all the sucess, health, and happiness in the world. May your dreams take flight and your heart be filled with endless joy.<br> Wishing you a day filled with love, laughter, and everything that makes you happiest. Happy Birthday Bestie! 🥳🎊 <br>I love you❤️🥹`;
+      `Happy Birthday, ${name}!<br><br> Thank you for always being that amazing person who brings joy to everyone around you. Your kindness, laughter, and warmth make the world a better place. Continue to shine and spread happiness wherever you go. May this year bring you countless blessings, unforgettable memories, and all the love your heart can hold.<br> On this day, I wish you all the sucess, health, and happiness in the world. May your dreams take flight and your heart be filled with endless joy. Happy Birthday Bestie! 🥳🎊 <br>I love you❤️🥹`;
   });
 
   // ---------- Partition 3: Flip Card ----------
